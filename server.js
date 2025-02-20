a0ec0ea029de98e446d2de054709586ad27cd0a0
camachoo18 <estivencamachops@gmail.com>
2025-02-20 13:01:34 +0100

    Implementacion auth google y github

--- server.js
+++ server.js
@@ -3,12 +3,15 @@ const express = require('express');
 const session = require('express-session');
 const passport = require('passport');
 const GitHubStrategy = require('passport-github2').Strategy;
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const app = express();
 const port = 3000;
 
 const CLIENT_ID_GITHUB = process.env.CLIENT_ID_GITHUB;
 const CLIENT_SECRET_GITHUB = process.env.CLIENT_SECRET_GITHUB;
+const CLIENT_ID_GOOGLE = process.env.CLIENT_ID_GOOGLE;
+const CLIENT_SECRET_GOOGLE = process.env.CLIENT_SECRET_GOOGLE;
 
 
 passport.use(new GitHubStrategy({
@@ -20,6 +23,16 @@ function(accessToken, refreshToken, profile, done) {
   return done(null, profile);
 }
 ));
+
+passport.use(new GoogleStrategy({
+    clientID: CLIENT_ID_GOOGLE,
+    clientSecret: CLIENT_SECRET_GOOGLE,
+    callbackURL: 'http://localhost:3000/auth/google/callback'
+  },
+  function(accessToken, refreshToken, profile, done) {
+    return done(null, profile);
+  }
+  ));
 
 passport.serializeUser(function(user, done) {
   done(null, user);
@@ -47,9 +60,12 @@ function ensureAuthenticated(req, res, next) {
 }
 
 app.get("/", (req, res) => {
-  const html = "<a href='/auth/github'>Autenticar con GitHub</a>";
+    const html = `
+      <a href='/auth/github'>Autenticar con GitHub</a><br>
+      <a href='/auth/google'>Autenticar con Google</a>
+    `;
     res.send(html);
-});
+  });
 
 
 app.get('/auth/github',
@@ -57,12 +73,24 @@ app.get('/auth/github',
   
 );
 
+// Ruta para iniciar la autenticación con Google
+app.get('/auth/google',
+    passport.authenticate('google', { scope: ['profile', 'email'] })
+  );
+
 app.get('/auth/github/callback',
     passport.authenticate('github', { failureRedirect: '/' }),
     (req, res, next) => {
         res.send('/profile');
     }
 );
+// Ruta de callback después de la autenticación con Google
+app.get('/auth/google/callback',
+    passport.authenticate('google', { failureRedirect: '/' }),
+    (req, res) => {
+      res.redirect('/profile');
+    }
+  );
 
 app.get('/profile', ensureAuthenticated, (req, res) => {
     res.send(`Hola ${req.user.username || req.user.displayName}`);
