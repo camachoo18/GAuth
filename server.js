ddca98747329278fd881f62bebdd1a5a35a337ae
camachoo18 <estivencamachops@gmail.com>
2025-02-20 10:04:58 +0100

    añado servidor con todas las dependencias

--- server.js
+++ server.js
@@ -7,35 +7,40 @@ const GitHubStrategy = require('passport-github2').Strategy;
 const app = express();
 const port = 3000;
 
-// Configuración de la sesión
-app.use(session({
-  secret: 'your_secret_key',
-  resave: false,
-  saveUninitialized: true
-}));
+const CLIENT_ID_GITHUB = process.env.CLIENT_ID_GITHUB;
+const CLIENT_SECRET_GITHUB = process.env.CLIENT_SECRET_GITHUB;
 
-// Inicializar passport
-app.use(passport.initialize());
-app.use(passport.session());
 
-// Configuración de la estrategia de GitHub
 passport.use(new GitHubStrategy({
-  clientID: process.env.GITHUB_CLIENT_ID,
-  clientSecret: process.env.GITHUB_CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/auth/github/callback"
+  clientID: CLIENT_ID_GITHUB,
+  clientSecret: CLIENT_SECRET_GITHUB,
+  callbackURL: 'http://localhost:3000/auth/github/callback'
 },
 function(accessToken, refreshToken, profile, done) {
-  // Aquí puedes manejar el perfil del usuario
   return done(null, profile);
-}));
+}
+));
 
-// Serializar y deserializar el usuario
-passport.serializeUser((user, done) => {
+passport.serializeUser(function(user, done) {
   done(null, user);
 });
 
-passport.deserializeUser((obj, done) => {
+passport.deserializeUser(function(obj, done) {
   done(null, obj);
+});
+
+app.use(session({ 
+    secret: "secret",
+    resave: false,
+    saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get("/", (req, res) => {
+  const html = "<a href='/auth/github'>Autenticar con GitHub</a>";
+    res.send(html);
 });
 
 // Ruta principal
@@ -43,19 +48,8 @@ app.get('/', (req, res) => {
   res.send('¡Hola, mundo!');
 });
 
-// Ruta de autenticación con GitHub
-app.get('/auth/github',
-  passport.authenticate('github', { scope: ['user:email'] })
-);
 
-// Ruta de callback de GitHub
-app.get('/auth/github/callback', 
-  passport.authenticate('github', { failureRedirect: '/' }),
-  (req, res) => {
-    // Autenticación exitosa
-    res.redirect('/');
-  }
-);
+
 
 // Iniciar el servidor
 app.listen(port, () => {
